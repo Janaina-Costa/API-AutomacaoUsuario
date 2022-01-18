810f30d2deb321beea193b329a509f52629c32e5
Janaina-Costa <janainacosta.dev@gmail.com>
2022-01-18 15:37:51 -0300

    Add typescript e incrementando desenvolvimento

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Executei');
+import express, {Request, Response, NextFunction} from  'express'
+
+const app = express()
+const porta = 3000
+const host = 'http://localhost:'
+
+app.get('/status', (req:Request, res:Response, next:NextFunction)=>{
+    res.status(200).send({foo:'Aeee porraaaa'})
+})
+
+app.listen(porta, ()=>console.log(`Executando em... ${host}${porta}`)
+)
